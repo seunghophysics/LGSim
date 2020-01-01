47b21d781bd77fb229fa9772b9813c032f67d95d
Seungho Han <han@icrr.u-tokyo.ac.jp>
2020-01-02 03:40:21 +0900

    Add mirror surface to LG.

--- src/LGSimDetectorConstruction.cc
+++ src/LGSimDetectorConstruction.cc
@@ -1,7 +1,10 @@
 #include "G4Box.hh"
+#include "G4LogicalBorderSurface.hh"
+#include "G4LogicalSkinSurface.hh"
 #include "G4LogicalVolume.hh"
 #include "G4NistManager.hh"
 #include "G4PVPlacement.hh"
+#include "G4OpticalSurface.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4Tubs.hh"
 #include "G4VisAttributes.hh"
@@ -21,7 +24,7 @@ G4VPhysicalVolume* LGSimDetectorConstruction::Construct()
     
     // Materials
     G4NistManager* nistManager = G4NistManager::Instance();
-    G4Material* vacuum = nistManager->FindOrBuildMaterial("G4_Galactic");
+    G4Material* vacuum = nistManager->FindOrBuildMaterial("G4_AIR");
     G4Material* leadglass = nistManager->FindOrBuildMaterial("G4_GLASS_LEAD");
     leadglass->SetMaterialPropertiesTable(lgMPT);
     
@@ -33,12 +36,18 @@ G4VPhysicalVolume* LGSimDetectorConstruction::Construct()
     // Lead-glass
     G4Box *lgBoxSolid = new G4Box("LGBoxSolid", 15.*cm, 5.*cm, 5.*cm);
     G4LogicalVolume* lgBoxLV = new G4LogicalVolume(lgBoxSolid, leadglass, "LGBoxLV");
-    new G4PVPlacement(G4Transform3D(), "LGBoxPV", lgBoxLV, worldPV, false, 0);
+    G4VPhysicalVolume* lgBoxPV = new G4PVPlacement(G4Transform3D(), "LGBoxPV", lgBoxLV, worldPV, false, 0);
     
     G4Tubs *lgTubeSolid = new G4Tubs("LGTubeSolid", 0, 3.5*cm, 1.5*cm, 0, 360.*deg);
     G4LogicalVolume* lgTubeLV = new G4LogicalVolume(lgTubeSolid, leadglass, "LGTubeLV");
     G4RotationMatrix rotLGTube; rotLGTube.rotateY(90.*deg); G4ThreeVector vLGTube(16.5*cm, 0, 0);
     new G4PVPlacement(G4Transform3D(rotLGTube, vLGTube), "LGTubePV", lgTubeLV, worldPV, false, 0, true);
+    
+    G4OpticalSurface* lgSurface = new G4OpticalSurface("LGSurface");
+    new G4LogicalBorderSurface("LGBorderSurface", lgBoxPV, worldPV, lgSurface);
+    lgSurface->SetType(dielectric_LUT);
+    lgSurface->SetModel(LUT);
+    lgSurface->SetFinish(polishedlumirrorair);
     
     // Visualization settings
     worldLV->SetVisAttributes(G4VisAttributes::Invisible);
